d57001ff34d91868651f9d26b2fe0fc119357acb
Brian Blosser <brian.blosser@fxinnovation.com>
2024-06-29 14:03:22 -0400

    Add react apps.

---
+++ apps/inventory/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="inventory" />
+    </div>
+  );
+}
+
+export default App;
--- apps/inventory/vite.config.ts
+++ apps/inventory/vite.config.ts
@@ -5,7 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/react-store',
+  cacheDir: '../../node_modules/.vite/apps/inventory',
 
   server: {
     port: 4200,
@@ -25,7 +25,7 @@ export default defineConfig({
   // },
 
   build: {
-    outDir: '../../build/react-store',
+    outDir: '../../dist/apps/inventory',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
