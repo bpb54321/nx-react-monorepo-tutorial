f65c04390d6d5d7a03af9d29ce9239f3d3d4c2a0
Brian Blosser <brian.blosser@fxinnovation.com>
2024-07-07 10:50:06 -0400

    Update the react-store unit test.

--- apps/react-store/src/app/app.spec.tsx
+++ apps/react-store/src/app/app.spec.tsx
@@ -1,15 +1,22 @@
 import { render } from '@testing-library/react';
 
 import App from './app';
+import { BrowserRouter } from 'react-router-dom';
+
+const WrappedApp = () => {
+  return <BrowserRouter>
+    <App />
+  </BrowserRouter>;
+}
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<App />);
+    const { baseElement } = render(<WrappedApp />);
     expect(baseElement).toBeTruthy();
   });
 
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Welcome react-store/gi)).toBeTruthy();
+  it('should have home as the title', () => {
+    const { getByText } = render(<WrappedApp />);
+    expect(getByText(/home/i)).toBeTruthy();
   });
 });
